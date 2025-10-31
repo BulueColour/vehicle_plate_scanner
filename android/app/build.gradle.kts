02640b45e8dcccbf6a73083d39149d66c3e787f4
BulueColour <monmon.9283@gmail.com>
2025-10-31 14:06:51 +0700

    Update vibrate and sound when detected with camera on

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 
 android {
     namespace = "com.example.vehicle_plate_scanner"
-    compileSdk = flutter.compileSdkVersion
+    namespace = "com.burhanrashid.flutter_vibrate"
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -26,7 +27,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
